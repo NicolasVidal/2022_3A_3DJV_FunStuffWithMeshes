e62c10e379a0896f425e98d1bd5234fae0757e2e
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-02-01 10:38:52 +0100

    Create quad

--- Assets/Scripts/MeshBuilders/QuadBuilderScript.cs
+++ Assets/Scripts/MeshBuilders/QuadBuilderScript.cs
@@ -2,32 +2,34 @@ using UnityEngine;
 
 namespace MeshBuilders
 {
-    public class TriangleBuilderScript : MonoBehaviour
+    public class QuadBuilderScript : MonoBehaviour
     {
         public MeshFilter mf;
     
         // Start is called before the first frame update
         private void Start()
         {
-            var mesh = CreateTriangle();
+            var mesh = CreateQuad();
 
             mf.mesh = mesh;
         }
 
-        private static Mesh CreateTriangle()
+        private static Mesh CreateQuad()
         {
             var mesh = new Mesh();
 
             mesh.vertices = new[]
             {
                 new Vector3(-0.5f, -0.5f, 0f), // A
-                new Vector3(0.5f, -0.5f, 0f), // B
-                new Vector3(0f, 0.5f, 0f), // C
+                new Vector3(-0.5f, 0.5f, 0f), // B
+                new Vector3(0.5f, 0.5f, 0f), // C
+                new Vector3(0.5f, -0.5f, 0f), // D
             };
 
             mesh.triangles = new[]
             {
-                0, 2, 1
+                0, 1, 2,
+                0, 2, 3,
             };
     
             return mesh;
